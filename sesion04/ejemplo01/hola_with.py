74e3201dcf95210dfbec89fee20b37e203a98593
José Juan Mota <backmota@TheLastJedi.local>
2020-05-26 17:03:37 -0500

    Clase Muestra

--- sesion04/ejemplo01/hola_with.py
+++
@@ -1,7 +0,0 @@
-with open("hola2.txt", 'w') as archivo: 
-    archivo.write("Primer linea de este archivo\n") 
-    archivo.writelines(['Multiples Lineas\n', 'En Lista']) 
-                                                                                                                                                 
-with open("hola2.txt", 'r') as archivo: 
-    print(archivo.readline()) 
-    print(archivo.readlines())  
--- sesion04/readme.md
+++
@@ -1,6 +0,0 @@
-## Manipulación de archivos, JSON, CSV y excepciones
-
-### Objetivos
-
-- Abrir, crear y editar archivos de textos con Python
-- Convertir diccionarios a JSON y viceversa, así como archivos
